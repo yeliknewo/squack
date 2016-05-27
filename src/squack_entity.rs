03929b9af2252235941da9878dbc7a7c0d4a11de
Kiley Owen <yelikdev@gmail.com>
2016-05-27 14:11:37 -0400

    Version 0.3.0, switched to macros

--- src/squack_entity.rs
+++ src/squack_entity.rs
@@ -8,6 +8,8 @@ pub struct SquackEntity {
     name: Option<Box<Name>>,
 }
 
+impl_component_for_entity!(SquackEntity, name, Name, set_option_name, set_name, with_name, get_name, get_mut_name, take_name, give_name);
+
 impl SquackEntity {
     pub fn new(id: Id) -> SquackEntity {
         SquackEntity {
@@ -18,44 +20,81 @@ impl SquackEntity {
         }
     }
 
+    #[inline]
+    pub fn set_option_renderable(&mut self, renderable: Option<Box<Renderable>>) {
+        self.renderable = renderable;
+    }
+
+    #[inline]
+    pub fn set_option_transform(&mut self, transform: Option<Box<Transform>>) {
+        self.transform = transform;
+    }
+
+    #[inline]
     pub fn set_renderable(&mut self, renderable: Renderable) {
-        self.renderable = Some(Box::new(renderable));
+        self.set_option_renderable(Some(Box::new(renderable)));
     }
 
+    #[inline]
     pub fn set_transform(&mut self, transform: Transform) {
-        self.transform = Some(Box::new(transform));
+        self.set_option_transform(Some(Box::new(transform)));
     }
 
-    pub fn get_name(&self) -> Option<&Box<Name>> {
-        self.name.as_ref()
+    #[inline]
+    pub fn with_renderable(mut self, renderable: Renderable) -> SquackEntity {
+        self.set_renderable(renderable);
+        self
     }
 
+    #[inline]
+    pub fn with_transform(mut self, transform: Transform) -> SquackEntity {
+        self.set_transform(transform);
+        self
+    }
+
+    #[inline]
+    pub fn take_renderable(&mut self) -> Option<Box<Renderable>> {
+        self.renderable.take()
+    }
+
+    #[inline]
     pub fn take_transform(&mut self) -> Option<Box<Transform>> {
         self.transform.take()
     }
 
+    #[inline]
+    pub fn give_renderable(&mut self, renderable: Box<Renderable>) {
+        self.set_option_renderable(Some(renderable));
+    }
+
+    #[inline]
     pub fn give_transform(&mut self, transform: Box<Transform>) {
-        self.transform = Some(transform);
+        self.set_option_transform(Some(transform));
     }
 }
 
 impl Entity<Id, SquackEntity> for SquackEntity {
+    #[inline]
     fn get_id(&self) -> Id {
         self.id
     }
 
+    #[inline]
     fn get_renderable(&self) -> Option<&Box<Renderable>> {
         self.renderable.as_ref()
     }
 
+    #[inline]
     fn get_transform(&self) -> Option<&Box<Transform>> {
         self.transform.as_ref()
     }
 
+    #[inline]
     fn get_mut_renderable(&mut self) -> Option<&mut Box<Renderable>> {
         self.renderable.as_mut()
     }
 
+    #[inline]
     fn get_mut_transform(&mut self) -> Option<&mut Box<Transform>> {
         self.transform.as_mut()
     }
@@ -68,6 +107,7 @@ impl Entity<Id, SquackEntity> for SquackEntity {
         }
     }
 
+    #[inline]
     fn is_tick(&self) -> bool {
         self.transform.is_some() && self.renderable.is_some()
     }
