b1a0b77e5de226457b47196e071be918e4faa62a
Kiley Owen <yelikdev@gmail.com>
2016-06-08 19:00:54 -0400

    Some Conventions

--- src/prefabs/mod.rs
+++ src/prefabs/mod.rs
@@ -5,3 +5,13 @@ mod player;
 pub use self::watcher::*;
 pub use self::ground::*;
 pub use self::player::*;
+
+/*
+Prefab naming conventions
+
+new_PREFAB_NAME returns an entity that is that prefabs
+
+add_PREFAB_NAME adds the entity to the world
+
+add_PREFAB_NAME_with_PROPERTY for non essiential properties
+*/
--- src/prefabs/player.rs
+++ src/prefabs/player.rs
@@ -7,8 +7,11 @@ use utils::names::*;
 use utils::collision_groups::*;
 use components::*;
 
+pub fn new_player(manager: &mut SNode, world: &mut SWorld, position: Vector2<Coord>) -> SEntity {
+    let width = 100.0;
+    let height = 50.0;
+    let mass = 1.0;
 
-pub fn new_player(manager: &mut SNode, world: &mut SWorld, position: Vector2<Coord>) {
     let id = manager.alloc().expect("Manager ran out of ids");
 
     let entity = SEntity::new(id, 0)
@@ -24,8 +27,8 @@ pub fn new_player(manager: &mut SNode, world: &mut SWorld, position: Vector2<Coo
                 ShapeHandle2::new(
                     Cuboid2::new(
                         Vector2::new(
-                            50.0,
-                            25.0
+                            width / 2.0,
+                            height / 2.0
                         )
                     )
                 ),
@@ -34,7 +37,7 @@ pub fn new_player(manager: &mut SNode, world: &mut SWorld, position: Vector2<Coo
         )
         .with_physics_obj(
             PhysicsObj::new(
-                1.0,
+                mass,
                 Vector2::new(0.0, 0.0),
                 1.0
             )
@@ -44,9 +47,9 @@ pub fn new_player(manager: &mut SNode, world: &mut SWorld, position: Vector2<Coo
                 0,
                 vec!(
                     Vector2::new(0.0, 0.0),
-                    Vector2::new(100.0, 0.0),
-                    Vector2::new(100.0, 50.0),
-                    Vector2::new(0.0, 50.0)
+                    Vector2::new(width, 0.0),
+                    Vector2::new(width, height),
+                    Vector2::new(0.0, height)
                 ),
                 [1.0, 1.0, 0.0, 1.0]
             )
@@ -57,5 +60,15 @@ pub fn new_player(manager: &mut SNode, world: &mut SWorld, position: Vector2<Coo
 
     world.get_mut_entity_by_name(WATCHER_NAME).expect("Watcher was none").get_mut_hit_watcher().expect("Watcher had no hit watcher").add_entity(&entity);
 
+    entity
+}
+
+pub fn add_player(manager: &mut SNode, world: &mut SWorld) {
+    let entity = new_player(manager, world, Vector2::new(0.0, 0.0));
+    world.add_entity(entity);
+}
+
+pub fn add_player_at_position(manager: &mut SNode, world: &mut SWorld, position: Vector2<Coord>) {
+    let entity = new_player(manager, world, position);
     world.add_entity(entity);
 }
